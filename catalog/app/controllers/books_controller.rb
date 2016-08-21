6a5d93062f2481be4d40cde256dddc9deaf244e0
Alexandre Corrêa Barbosa <abarbos@thoughtworks.com>
2016-08-20 23:35:49 -0300

    Salva livro e exibe dados do livro salvo

--- catalog/app/controllers/books_controller.rb
+++ catalog/app/controllers/books_controller.rb
@@ -3,5 +3,17 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to @book
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
   end
 end
