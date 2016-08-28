d894a22a0fd27158a05b809384ce4a87d8f9af70
Alexandre Corrêa Barbosa <abarbos@thoughtworks.com>
2016-08-28 20:16:52 -0300

    Atualização de livro

--- catalog/app/controllers/books_controller.rb
+++ catalog/app/controllers/books_controller.rb
@@ -17,6 +17,16 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to @book
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :author)
